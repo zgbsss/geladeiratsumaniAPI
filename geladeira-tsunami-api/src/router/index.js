a521d4554eb24572fac357413ca13c1060ddae35
zgbsss <99218597+zgbsss@users.noreply.github.com>
2022-10-31 09:14:44 -0300

    Add files via upload

---
+++ geladeira-tsunami-api/src/router/index.js
@@ -0,0 +1,42 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import YunliView from '../views/YunliView.vue'
+import DprianView from '../views/DprianView.vue'
+import DprliveView from '../views/DprliveView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/callback',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/yunli',
+      name: 'yungli',
+      component: YunliView
+    },
+    {
+      path: '/dprian',
+      name: 'dprian',
+      component: DprianView
+    },
+    {
+      path: '/dprlive',
+      name: 'dprlive',
+      component: DprliveView
+    }
+  ]
+})
+
+export default router
---
+++ geladeira-tsunami-api/src/views/NonlyView.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      token: "dddd",
+      dados: {},
+      albums: [],
+      artista2: {}
+    };
+  },
+  methods: {
+    getHashParams() {
+      var hashParams = {};
+      var e,
+        r = /([^&;=]+)=?([^&;]*)/g,
+        q = this.token;
+      e = r.exec(q);
+      while (e) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+        e = r.exec(q);
+      }
+      return hashParams;
+    },
+  },
+  async created() {
+    this.token = this.$route.query["access_token"];
+
+    response = await axios.get('https://api.spotify.com/v1/artists/3ZHU5AKrUmIPnCFfr82QER', { headers: {
+      Authorization: `Bearer ${this.token}`} 
+    })
+    this.artista2 = response.data
+    // console.log(this.getHashParams());
+  },
+};
